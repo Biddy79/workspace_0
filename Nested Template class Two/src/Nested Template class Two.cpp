761f5285f4d168dc564c18ba404a7026cc3a1aba
Adam <a.beardsley79@gmail.com>
2019-06-03 17:14:22 +0100

    New attempt at Nested Template class

---
+++ Nested Template class Two/src/Nested Template class Two.cpp
@@ -0,0 +1,24 @@
+//============================================================================
+// Name        : Nested.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    /*ring<string> textring;
+
+	textring.add("One");
+	textring.add("Two");
+	textring.add("Three");
+	textring.add("Four");*/
+
+
+
+	return 0;
+}
