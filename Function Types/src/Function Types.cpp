8d817a8f15d9047462d79e8c41b6762ad575c226
Adam <a.beardsley79@gmail.com>
2019-07-16 17:59:02 +0100

    function type project updated

--- Function Types/src/Function Types.cpp
+++ Function Types/src/Function Types.cpp
@@ -13,6 +13,22 @@
 
 using namespace std;
 
+//using check function as funtion pointer
+bool check(string& test){
+	return test.size() == 3;
+}
+
+class Check{
+public:
+	//functor overloaded() operator //
+	bool operator()(string& test){
+		return test.size() == 5;
+	}
+}
+//instantiateing and object of Check class to be used as paramitor in count_if function below
+check1;
+
+
 int main() {
 
 
@@ -20,10 +36,19 @@ int main() {
 
 	vector<string> vec {"one", "two", "three"};
 
+	//pasing lambda to count_if function
 	int count = count_if(vec.begin(), vec.end(),
 			[size](string test){return test.size() == size;});
-
 	cout << count << endl;
+
+	//passing function pointer check to count_if function
+	count = count_if(vec.begin(), vec.end(), check);
+	cout << count << endl;
+
+	count = count_if(vec.begin(), vec.end(), check1);
+	cout << count << endl;
+
+
 
 	return 0;
 }
