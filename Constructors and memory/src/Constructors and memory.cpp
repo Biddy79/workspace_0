15e164c0e058dfb478f38365304c8d539f9aeefa
Adam <a.beardsley79@gmail.com>
2019-08-11 17:05:40 +0100

    Constructors and memory project added

---
+++ Constructors and memory/src/Constructors and memory.cpp
@@ -0,0 +1,20 @@
+//============================================================================
+// Name        : Constructors.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include "Test.h"
+using namespace std;
+
+int main() {
+
+	Test t1;
+	cout << t1 << endl;
+
+
+	return 0;
+}
