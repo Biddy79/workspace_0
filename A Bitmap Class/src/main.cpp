cd368edbe15d74af974ae9ec16c49a4530930aba
Adam <a.beardsley79@gmail.com>
2019-10-09 15:15:18 +0100

    scaling added

--- A Bitmap Class/src/main.cpp
+++ A Bitmap Class/src/main.cpp
@@ -19,12 +19,22 @@ int main() {
 
 	//bitmap.setPixel(WIDTH/2, HEIGHT/2, 255, 255, 255);
 
+	double min = 999999;
+	double max = -999999;
+
 
 	for(int y = 0; y < HEIGHT; y++){
 		for(int x = 0; x < WIDTH; x++){
-			bitmap.setPixel(x, y, 255, 0, 0);
+			double xFractal = (x - WIDTH/2) * 2.0/WIDTH;
+			double yFractal = (y - HEIGHT/2) * 2.0/HEIGHT;
+
+			if(yFractal < min) min = yFractal;
+			if(yFractal > max) max = yFractal;
+
 		}
 	}
+
+	cout << min << ", " << max << endl;
 
 	bitmap.write("test.bmp");
 
